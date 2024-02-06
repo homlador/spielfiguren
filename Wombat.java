0c77b8699e43b437747146a20e9830fd80d38749
Mirko Zieger <mirko-zieger@ge-walsum.de>
2024-02-06 20:12:01 +0100

    Klasse Wombat um Attribut fellSauberkeit ergänzt, Datentyp des Attributs geändert, Konstruktor ergänzt, Methode zur Änderung ergänzt

--- Wombat.java
+++ Wombat.java
@@ -8,6 +8,8 @@
 public class Wombat extends Spielfigur
 {
     private String fell;
+    //fellSauberkeit geändert als String, damit diese Eigenschaft ausgegeben werden kann
+    private String fellSauberkeit;
 
     /**
      * Konstruktor fuer Objekte der Klasse WOMBAT
@@ -18,7 +20,8 @@ public class Wombat extends Spielfigur
         // Damit werden die Werte der Attribute Stärke, Geschwindigkeit und Geschicklichkeit gesetzt.
         
         // Nun die Initalisierung der Attribute der Unterklasse
-        fell = "kuschelig";         
+        fell = "kuschelig"; 
+        fellSauberkeit = "sauber";
     }
 
     /**
@@ -31,19 +34,35 @@ public class Wombat extends Spielfigur
     public String charakterbeschreibungAusgeben()
     {
         return "Wombats sind kleine Artgenossen mit kuscheligem Fell, die es aber auch in sich haben!";
-        
     }
     
     /**
      * Gibt die Spielereigenschaften auf dem Ausgabefenster aus.
      * �berschreibt die Methode der Oberklasse
      */
+    //mitverändert: Syso zu Fellsauberkeit
     public void SpielerEigenschaftenAusgeben()
     {
         System.out.println("Fell: "+ fell);
+        System.out.println("Das Fell ist: "+fellSauberkeit);
         // Aufruf der gleichnamigen Methode der Oberklasse
         super.spielerEigenschaftenAusgeben();
     }
  
+    /**
+     * Gibt dem Wombat die Möglichkeit, sein Fell dreckig zu machen
+     * (z.B. bei der Nahrungssuche)
+     */
+    //evtl umbenennen, obere Beschreibung ändern
+    public void fellDreckigMachen(){
+        fellSauberkeit = "dreckig";
+    }
+    
+    /**
+     * Gibt dem Wombat die Möglickeit, sein Fell zu säubern
+     */
+    public void fellSäubern(){
+        fellSauberkeit = "sauber";
+    }
     
 }
